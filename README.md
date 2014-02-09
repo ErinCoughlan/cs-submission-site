30818604530afa59613cb8e9308a2f9bf21458b1
erinkidd01 <erinkidd01@gmail.com>
2014-02-09 12:42:13 -0800

    Added new login page

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ cs-submission-site
 ==================
 
 Harvey Mudd College CS Submission Site.
+
+Currently running at cs.hmc.edu/~ecoughlan/submit
---
+++ frontend/eggs.js
@@ -0,0 +1,25 @@
+var alienLoc = "img/alien.png";
+
+/** Creates a new alien in a random location on the screen */
+function createAlien() {
+	// Subtract 100, which is approx. alien size
+	var x = randInt(1, $(window).width()-100);
+	var y = randInt(1, $(window).height()-100);
+
+	var alien = document.createElement('img');
+	alien.src = alienLoc;
+
+	// CSS stuff for positioning the new alien
+	alien.style.position = 'absolute';
+	alien.style.top = y;
+	alien.style.left = x;
+	alien.className = 'alien';
+
+	var src = document.getElementById('alienHome');
+	src.appendChild(alien);
+};
+
+/** Returns a random integer between start and end */
+function randInt(start, end) {
+	return Math.floor(Math.random() * (1 + end - start)) + start;
+};
