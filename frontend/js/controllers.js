ec1acd99c4051f3e8897af07ae07a45d825d9dbf
Luke Sedney <lukesedney@gmail.com>
2014-02-21 21:02:16 -0800

    populate left column with angular

---
+++ frontend/js/controllers.js
@@ -0,0 +1,16 @@
+
+var submissionApp = angular.module('submissionApp', []);
+
+submissionApp.controller('StudentCtrl', function ($scope, $http) {
+    
+    // get the list of assignments from jsonstub.
+    $http.get('http://jsonstub.com/spring2014/cs130/assignments', {
+        headers: {
+            'JsonStub-User-Key': '2d70b72a-502e-4fbd-9a8f-c9246b9d0fff',
+            'JsonStub-Project-Key': 'cc74f36f-3235-4d8b-bfdc-4a94cdedad45'
+        }
+    }).success(function (data) {
+        $scope.assignments = data;
+    });
+    
+});
