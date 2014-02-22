48908245bb17042e70bb7158f3acfe4edcf368c3
Luke Sedney <lukesedney@gmail.com>
2014-02-22 14:52:26 -0800

    use strict mode

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -1,16 +1,26 @@
+//
+//  Spring 2014 - Erin Coughlan & Philip Davis & Luke Sedney
 
-var submissionApp = angular.module('submissionApp', []);
+// for the benefit of JSLint:
+/*global angular*/
 
-submissionApp.controller('StudentCtrl', function ($scope, $http) {
+(function () {
+    "use strict";
     
-    // get the list of assignments from jsonstub.
-    $http.get('http://jsonstub.com/spring2014/cs130/assignments', {
-        headers: {
-            'JsonStub-User-Key': '2d70b72a-502e-4fbd-9a8f-c9246b9d0fff',
-            'JsonStub-Project-Key': 'cc74f36f-3235-4d8b-bfdc-4a94cdedad45'
-        }
-    }).success(function (data) {
-        $scope.assignments = data;
+    var submissionApp = angular.module('submissionApp', []);
+
+    submissionApp.controller('StudentCtrl', function ($scope, $http) {
+
+        // get the list of assignments from jsonstub.
+        $http.get('http://jsonstub.com/spring2014/cs130/assignments', {
+            headers: {
+                'JsonStub-User-Key': '2d70b72a-502e-4fbd-9a8f-c9246b9d0fff',
+                'JsonStub-Project-Key': 'cc74f36f-3235-4d8b-bfdc-4a94cdedad45'
+            }
+        }).success(function (data) {
+            $scope.assignments = data;
+        });
+
     });
     
-});
+}());
