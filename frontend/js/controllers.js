ae85a303d26dc82fab5c9ed8c96ddd1e96abd921
erinkidd01 <erinkidd01@gmail.com>
2014-02-25 16:45:00 -0800

    Config file for angular calls

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -8,28 +8,26 @@
     "use strict";
     
     var submissionApp = angular.module('submissionApp', []);
+    var config = {
+            headers: {
+                'JsonStub-User-Key': '2d70b72a-502e-4fbd-9a8f-c9246b9d0fff',
+                'JsonStub-Project-Key': 'cc74f36f-3235-4d8b-bfdc-4a94cdedad45'
+            }
+        };
 
     submissionApp.controller('StudentCtrl', function ($scope, $http) {
 
         // get the list of assignments from jsonstub.
-        $http.get('http://jsonstub.com/spring2014/cs130/assignments', {
-            headers: {
-                'JsonStub-User-Key': '2d70b72a-502e-4fbd-9a8f-c9246b9d0fff',
-                'JsonStub-Project-Key': 'cc74f36f-3235-4d8b-bfdc-4a94cdedad45'
-            }
-        }).success(function (data) {
-            $scope.assignments = data;
+        $http.get('http://jsonstub.com/spring2014/cs130/assignments', config).success(
+            function (data) {
+                $scope.assignments = data;
         });
 
 
         // get the list of files for the first assignment from jsonstub
-        $http.get('http://jsonstub.com/spring2014/cs130/assignment0', {
-            headers: {
-                'JsonStub-User-Key': '2d70b72a-502e-4fbd-9a8f-c9246b9d0fff',
-                'JsonStub-Project-Key': 'cc74f36f-3235-4d8b-bfdc-4a94cdedad45'
-            }
-        }).success(function (data) {
-            $scope.assignment0 = data;
+        $http.get('http://jsonstub.com/spring2014/cs130/assignment/0', config).success(
+            function (data) {
+                $scope.assignment0 = data;
         });
 
     });
