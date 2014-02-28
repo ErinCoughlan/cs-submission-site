b03133eefb1471872564c7b9e6909d2ca9c00157
Luke Sedney <lukesedney@gmail.com>
2014-02-27 16:48:36 -0800

    dynamically load the selected assignment (but just load assignment 0 every time for now because the other don't exist)

---
+++ frontend/js/app.js
@@ -0,0 +1,55 @@
+//
+//  Spring 2014 - Erin Coughlan & Philip Davis & Luke Sedney
+
+// for the benefit of JSLint:
+/*global angular*/
+
+var submissionApp = angular.module('submissionApp', ['ngRoute']);
+
+(function(){
+  "use strict";
+
+    // define routes
+    submissionApp.config(function($routeProvider, $locationProvider) {
+        $routeProvider.when('/assignments/:assignmentId', {
+            templateUrl: 'partials/assignment.html',
+            controller: 'AssignmentCtrl',
+            controllerAs: 'assignment'
+        });
+        $routeProvider.otherwise({redirectTo: '/assignments/3'});
+    }); 
+    
+})();
+
+/**
+ * Performs a mock submission for any assignments that were uploaded.
+ * DO NOT USE: SECURITY FLAWS
+ * (and horrendous style)
+ */
+function mockSubmit(e) {
+    e.preventDefault();
+
+    // Printing out the files that were uploaded
+    var inputs = document.querySelectorAll('input[type=file]');
+    for (var i= 0; i < inputs.length; i++) {
+        var input = inputs[i];
+
+        // Locate the first cell in the row
+        var rowId = input.parentNode.parentNode;
+        var cells = rowId.getElementsByTagName('td');
+
+        // Determine if any files were uploaded
+        var files = input.files;
+        if (files && cells.length >= 1) {
+            for (var j = 0; j < files.length; j++) {
+                // This is a security risk and a major flaw, but it will work for the demo
+                if (cells[0].innerHTML.indexOf('\n(submitted)') == -1) {
+                    cells[0].innerHTML += "\n(submitted)";
+                }
+            }
+        }
+    }
+
+    var form = document.getElementById('form-submit');
+    form.reset();
+}
--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -2,68 +2,52 @@
 //  Spring 2014 - Erin Coughlan & Philip Davis & Luke Sedney
 
 // for the benefit of JSLint:
-/*global angular*/
+/*global submissionApp*/
 
-(function () {
+
+(function(){
     "use strict";
-    
-    var submissionApp = angular.module('submissionApp', []);
-    var config = {
-            headers: {
-                'JsonStub-User-Key': '2d70b72a-502e-4fbd-9a8f-c9246b9d0fff',
-                'JsonStub-Project-Key': 'cc74f36f-3235-4d8b-bfdc-4a94cdedad45'
-            }
-        };
 
-    submissionApp.controller('StudentCtrl', function ($scope, $http) {
+    // define jsonstub headers
+    var config = {
+        headers: {
+            'JsonStub-User-Key': '2d70b72a-502e-4fbd-9a8f-c9246b9d0fff',
+            'JsonStub-Project-Key': 'cc74f36f-3235-4d8b-bfdc-4a94cdedad45'
+        }
+    };
+
+    submissionApp.controller('StudentCtrl', function ($scope, $http, $route, $routeParams, $location) {
+        this.$route = $route;
+        this.$location = $location;
+        this.$routeParams = $routeParams;
 
         // get the list of assignments from jsonstub.
         $http.get('http://jsonstub.com/spring2014/cs130/assignments', config).success(
             function (data) {
                 $scope.assignments = data;
-        });
+            }
+        );
 
+        // indicates whether the path is currently active
+        $scope.isActive = function(path) {
+            if ($location.path().substr(0, path.length) == path) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+    });
+
+    submissionApp.controller('AssignmentCtrl', function ($scope, $http, $routeParams) {
+        this.params = $routeParams;
 
         // get the list of files for the first assignment from jsonstub
+        // TODO: change from 'assignment/0' to  'assignment/' + this.params.assignmentId 
         $http.get('http://jsonstub.com/spring2014/cs130/assignment/0', config).success(
             function (data) {
-                $scope.assignment0 = data;
-        });
-
-    });
-    
-}());
-
-
-/**
- * Performs a mock submission for any assignments that were uploaded.
- * DO NOT USE: SECURITY FLAWS
- * (and horrendous style)
- */
-function mockSubmit(e) {
-    e.preventDefault();
-
-    // Printing out the files that were uploaded
-    var inputs = document.querySelectorAll('input[type=file]');
-    for (var i= 0; i < inputs.length; i++) {
-        var input = inputs[i];
-
-        // Locate the first cell in the row
-        var rowId = input.parentNode.parentNode;
-        var cells = rowId.getElementsByTagName('td');
-
-        // Determine if any files were uploaded
-        var files = input.files;
-        if (files && cells.length >= 1) {
-            for (var j = 0; j < files.length; j++) {
-                // This is a security risk and a major flaw, but it will work for the demo
-                if (cells[0].innerHTML.indexOf('\n(submitted)') == -1) {
-                    cells[0].innerHTML += "\n(submitted)";
-                }
+                $scope.assignment = data;
             }
-        }
-    }
+        );
+    });
 
-    var form = document.getElementById('form-submit');
-    form.reset();
-};
+})();
