5da45659876ae263ab7c3e67596b572995c2c066
Luke Sedney <lukesedney@gmail.com>
2014-03-06 03:22:11 -0800

    fixed the giant aliens

--- frontend/js/eggs.js
+++ frontend/js/eggs.js
@@ -5,6 +5,7 @@ function createAlien() {
 	// Subtract 100, which is approx. alien size
 	var x = randInt(1, $(window).width()-100);
 	var y = randInt(1, $(window).height()-100);
+
 	var alien = document.createElement('img');
 	alien.src = alienLoc;
 
@@ -16,9 +17,9 @@ function createAlien() {
 
 	var src = document.getElementById('alienHome');
 	src.appendChild(alien);
-}
+};
 
 /** Returns a random integer between start and end */
 function randInt(start, end) {
 	return Math.floor(Math.random() * (1 + end - start)) + start;
-}
+};
