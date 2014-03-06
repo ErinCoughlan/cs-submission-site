988ce4540011c4cbbd4fd30bff0fdad1561c7161
erinkidd01 <erinkidd01@gmail.com>
2014-03-05 22:12:53 -0800

    Server set up to render html pages

--- backend/submit/app.js
+++ backend/submit/app.js
@@ -8,12 +8,13 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var frontBase = __dirname + '/../../frontend/';
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(frontBase, 'html'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
@@ -23,16 +24,20 @@ app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(frontBase));
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', function(req, res){
+    res.render('index');
+});
+app.get('/cs5', function(req, res){
+    res.render('student');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
