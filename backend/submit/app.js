67b3dbf52021331a2b24800606b963dac2a4f83a
erinkidd01 <erinkidd01@gmail.com>
2014-03-06 23:20:59 -0800

    Added passport for login -- no users yet

--- backend/submit/app.js
+++ backend/submit/app.js
@@ -10,6 +10,28 @@ var http = require('http');
 var path = require('path');
 var frontBase = __dirname + '/../../frontend/';
 
+// Login configuration
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password'
+  },
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username or password.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect username or password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
 var app = express();
 
 // all environments
@@ -65,6 +87,11 @@ app.get('/', function(req, res){
 app.get('/cs5', function(req, res){
     res.render('student');
 });
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/cs5',
+                                   failureRedirect: '/',
+                                   failureFlash: true })
+);
 
 
 
