7ce34a3b1303c6d36cf71c84163e93afbe595fc2
erinkidd01 <erinkidd01@gmail.com>
2014-03-11 02:24:38 -0700

    Removed sketchy mockSubmit method

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -20,38 +20,6 @@ var submissionApp = angular.module('submissionApp', ['ngRoute']);
 
 })();
 
-/**
- * Performs a mock submission for any assignments that were uploaded.
- * DO NOT USE: SECURITY FLAWS
- * (and horrendous style)
- */
-function mockSubmit(e) {
-    e.preventDefault();
-
-    // Printing out the files that were uploaded
-    var inputs = document.querySelectorAll('input[type=file]');
-    for (var i= 0; i < inputs.length; i++) {
-        var input = inputs[i];
-
-        // Locate the first cell in the row
-        var rowId = input.parentNode.parentNode;
-        var cells = rowId.getElementsByTagName('td');
-
-        // Determine if any files were uploaded
-        var files = input.files;
-        if (files && cells.length >= 1) {
-            for (var j = 0; j < files.length; j++) {
-                // This is a security risk and a major flaw, but it will work for the demo
-                if (cells[0].innerHTML.indexOf('\n(submitted)') == -1) {
-                    cells[0].innerHTML += "\n(submitted)";
-                }
-            }
-        }
-    }
-
-    var form = document.getElementById('form-submit');
-    form.reset();
-};
 
 function submit(e) {
     e.preventDefault();
@@ -76,4 +44,8 @@ function submit(e) {
             }
         }
     }
+
+    // Replace this with an actual submit call
+    var form = document.getElementById('form-submit');
+    form.reset();
 };
