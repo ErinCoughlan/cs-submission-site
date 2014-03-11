679ac7b1eda2a3772f5638767c8ab26bfc684d69
Philip Davis <pdavis@hmc.edu>
2014-03-10 23:43:30 -0700

    Added a janky route for changing email

--- backend/submit/app.js
+++ backend/submit/app.js
@@ -97,7 +97,21 @@ app.post('/login',
                                    failureFlash: true })
 );
 
-
+app.post('/changemail', function(req, res) {
+    if(req.isAuthenticated()) {
+        user = req.user;
+        user.email = req.email;
+        
+        user.save(function(err) {
+            if(err) {
+                console.log("Error saving user email");
+                throw err;
+            }
+            return done(null, user);
+        });
+    }
+});
+         
 app.post('/signup', passport.authenticate('local-signup', {
     successRedirect : '/', // redirect to the secure profile section
     failureRedirect : '/signup', // redirect back to the signup page if there is an error
