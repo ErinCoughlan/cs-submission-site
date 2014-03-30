bbd0411b8335f13d1710ad2f5243bd06353cb8e4
erinkidd01 <erinkidd01@gmail.com>
2014-03-29 23:17:52 -0700

    Student schema and wishful thinking grader table view

---
+++ backend/submit/models/student.js
@@ -0,0 +1,26 @@
+// app/models/student.js
+var mongoose = require('mongoose');
+
+// define the schema for our assignment model
+// ObjectId allows us to reference other objects
+var ObjectId = mongoose.Schema.ObjectId;
+var studentSchema = mongoose.Schema({
+        name: String,
+        files: [{
+            // TODO: make into a real type if it seems neccessary
+            id: String,
+            name: String,
+            point: Number,
+            grade: Number,
+            comment: String,
+            // TODO
+            // partners: [ObjectId],
+            submissions: [{
+                id: ObjectId,
+                date: Date
+            }]
+        }]
+    });
+
+// create the model expose it to our app
+module.exports = mongoose.model('Student', studentSchema);
