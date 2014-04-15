a7999fccef0b0440515dfb93da52712d1bd9364f
Philip Davis <pdavis@hmc.edu>
2014-04-15 00:03:38 -0700

    Get grading page with data

--- backend/submit/routes/grade.js
+++ backend/submit/routes/grade.js
@@ -15,11 +15,11 @@ module.exports = function(app, passport) {
 
     // TODO validate that it's a grutor for the class
     app.post("/course/:course/assignment/:assignment/student/:student/file/:file/grade", isLoggedIn, function(req, res) {
-        var grader         = req.session.passport.user;
+        var graderUser         = req.session.passport.user;
         var courseName     = req.params.course;
-        var assignmentName = req.params.assignments;
+        var assignmentNumber = req.params.assignments;
         var studentName    = req.params.student;
-        var fileName       = req.params.file;
+        var fileNumber       = req.params.file;
 
         // Hit the database for all the things we need to save this. Cry inside.
         Course.findOne({"name": courseName}, function(err, course) {
@@ -27,52 +27,82 @@ module.exports = function(app, passport) {
                 res.send("error getting course");
                 return;
             }
+            
+            var assignments = course.assignments;
 
-            Assignment.findOne({"course": course.id_, "name": assignmentName}, function(err, assignment) {
-                if (err) {
-                    res.send("error getting assignment");
-                    return;
-                }
+            // TODO this will break if an assignment is deleted (not sure if it
+            // matters, arguably then we should just never see this page anyway)
+            var assignment = course.assignments[assignmentNumber];
 
-                Student.findOne({"name": studentName, "course_id": course.id_}, function(err, student) {
-                    if (err) {
-                        res.send("error getting student");
-                        return;
-                    }
-
-                    FileTemplate.findOne({"name": fileName, "assignment": assignment.id_}, function(err, fileTemplate) {
-                        if(err) {
-                            res.send("error getting filetemplate");
-                            return;
-                        }
-
-                        File.findOne({"template": fileTemplate.id_, "owner": student.id_}, function(err, file) {
-                            if(err) {
-                                res.send("error getting file");
-                                return;
-                            }
-
-                            file.graderCommens = req.body.comments;
-                            file.grade         = req.body.grade;
-
-                            Grader.findOne({"user_id": grader, "course_id": course.id_}, function(err, grader) {
-                                if(err) {
-                                    res.send("error getting grader object from user");
-                                    return;
-                                }
-
-                                file.grader = grader.id_;
-                                file.save();
-                            });
+            Grader.find({"course_id": course._id, "name": graderUser.name}, 
+                        function(err, grader) {
+                            Student.find({"files": {$exists: true}, "course_id": course.id,
+                                          "name": studentName}, 
+                                         function(err, student) {
+                                             var file;
+                                             student.files.forEach(function(aFile) {
+                                                 if(aFile.assignment == assignmentNumber &&
+                                                    aFile.template == fileNumber) {
+                                                     file = aFile;
+                                                 }
+                                             });
+                                             
+                                         });
                         });
-                    });
-                });
-            });
-
         });
     });
 
-};
+
+
+    // TODO validate that it's a grutor for the class
+    app.get("/course/:course/assignment/:assignment/student/:student/file/:file/grade", isLoggedIn, function(req, res) {
+        var graderUser         = req.session.passport.user;
+        var courseName     = req.params.course;
+        var assignmentNumber = req.params.assignments;
+        var studentName    = req.params.student;
+        var fileNumber       = req.params.file;
+
+        // Hit the database for all the things we need to save this. Cry inside.
+        Course.findOne({"name": courseName}, function(err, course) {
+            if (err) {
+                res.send("error getting course");
+                return;
+            }
+            
+            var assignments = course.assignments;
+
+            // TODO this will break if an assignment is deleted (not sure if it
+            // matters, arguably then we should just never see this page anyway)
+            var assignment = course.assignments[assignmentNumber];
+
+            Grader.find({"course_id": course._id, "name": graderUser.name}, 
+                        function(err, grader) {
+                            Student.find({"files": {$exists: true}, "course_id": course.id,
+                                          "name": studentName}, 
+                                         function(err, student) {
+                                             var file;
+                                             student.files.forEach(function(aFile) {
+                                                 if(aFile.assignment == assignmentNumber &&
+                                                    aFile.template == fileNumber) {
+                                                     file = aFile;
+                                                 }
+                                             });
+
+                                             data = {
+                                                 "file": file,
+                                                 "student": student,
+                                                 "course": course,
+                                                 "grader": grader
+                                             }
+                                             
+                                         });
+                        });
+        });
+    });
+
+
+});
+
 
 function isLoggedIn(req, res, next) {
 
