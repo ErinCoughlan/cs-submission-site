2071b23cddc6197dca20379c46339486378f2fbd
erinkidd01 <erinkidd01@gmail.com>
2014-04-24 19:42:18 -0700

    creating new courses and adding professors work

--- backend/submit/app.js
+++ backend/submit/app.js
@@ -712,14 +712,14 @@ function copyFile(source, target, cb) {
 
 // Add a new course
 app.post("/addCourse", function(req, res) {
-    courseName = req.body.course;
+    coursename = req.body.course;
     username = req.body.professor;
-    console.log("course name: " + courseName);
+    console.log("course name: " + coursename);
     console.log("professor name: " + username);
 
     // Create the course
     Course.findOne({
-        "name": courseName
+        "name": coursename
     }, function(err, course) {
         if (course) {
             console.log("course already created");
@@ -729,39 +729,40 @@ app.post("/addCourse", function(req, res) {
         course = new Course();
         course.name = courseName;
         course.save();
+
+        // Add the professor
+        User.findOne({
+                "local.username": username
+            }, function(err, user) {
+                if (!user) {
+                    user = new User();
+                    user.local.username = username;
+                    user.local.password = user.generateHash("asdf");
+                    user.local.email = "placeholder@cs.hmc.edu";
+                    user.save();
+                }
+
+                // Make the user a professor for the course
+                Professor.findOne({
+                        "user_id": user._id,
+                        "course_id": course._id
+                    }, function(err, professor) {
+                        if (professor) {
+                            console.log("professor already added");
+                            return;
+                        }
+
+                        professor = new Professor();
+                        professor.course_id = course._id;
+                        professor.user_id = user._id;
+                        professor.name = username;
+                        professor.save();
+                        user.professors.push(professor._id);
+                        user.save();
+                    });
+            });
     });
 
-    // Add the professor
-    User.findOne({
-            "local.username": username
-        }, function(err, user) {
-            if (!user) {
-                user = new User();
-                user.local.username = username;
-                user.local.password = user.generateHash("asdf");
-                user.local.email = "placeholder@cs.hmc.edu";
-                user.save();
-            }
-
-            // Make the user a professor for the course
-            Professor.findOne({
-                    "user_id": user._id,
-                    "course_id": course._id
-                }, function(err, professor) {
-                    if (professor) {
-                        console.log("professor already added");
-                        return;
-                    }
-
-                    professor = new Professor();
-                    professor.course_id = course._id;
-                    professor.user_id = user._id;
-                    professor.name = username;
-                    professor.save();
-                    user.professors.push(professor._id);
-                    user.save();
-                });
-        });
     res.redirect("/cs5");
 });
 
